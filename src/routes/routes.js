b52b729afced609b476c7fe6b5b9e9776a009cca
LeandroCastro <leandro_castro980@hotmail.com>
2024-04-26 15:45:53 -0300

    backend

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,25 @@
-import express from 'express';
-import Admin from '../models/admin.js';
-import Users from '../models/sigup.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import multer from 'multer';
+import express from 'express'
+import Admin from  '../models/admin.js'
+import Users from '../models/sigup.js'
+import bcrypt from 'bcrypt'
+import jwt from  'jsonwebtoken'
+import multer from 'multer'
 
 const router = express.Router();
 
-// Lógica para decodificar o token e extrair o id do usuário
-function decodeAuthToken(token) {
-  try {
-    const decoded = jwt.verify(token, 'suaChaveSecreta');
-    return decoded.userId;
-  } catch (error) {
-    throw new Error('Erro ao decodificar o token de autenticação');
-  }
-}
 
 // LOGIN PARA ADMIN //
 
 router.post('/register-admin', async (req, res) => {
+  
   try {
-    const { email, password } = req.body;
-    const existingAdmin = await Admin.findOne({ email });
+    
+    const { email, password} = req.body
 
-    if (existingAdmin) {
-      return res.status(400).json({ message: 'Email já está em uso' });
+    const existingAdmin = await Admin.findOne({ email })
+
+    if(existingAdmin) {
+      return res.status(400).json({message: 'Email já está em uso'})
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -33,21 +27,22 @@ router.post('/register-admin', async (req, res) => {
     const newAdmin = new Admin({
       email: email,
       password: hashedPassword
-    });
+    })
 
-    await newAdmin.save();
+    await newAdmin.save()
     res.status(201).json({ message: 'Administrador registrado com sucesso' });
+
   } catch (error) {
     console.error('Erro ao registrar administrador:', error);
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
-});
+})
 
 router.post('/login-admin', async (req, res) => {
   try {
     const { email, password } = req.body;
-    const admin = await Admin.findOne({ email });
 
+    const admin = await Admin.findOne({ email });
     if (!admin) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
@@ -56,14 +51,16 @@ router.post('/login-admin', async (req, res) => {
     if (!passwordMatch) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
-
+  
     const token = jwt.sign({ adminId: admin._id }, 'seuSegredo');
+
     res.status(200).json({ token });
   } catch (error) {
     console.error('Erro ao fazer login de administrador:', error);
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
+
 
 // ROTAS PARA O LOGIN DO USUÁRIO //
 
@@ -111,8 +108,8 @@ router.post('/register', upload.single("file"), async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await Users.findOne({ email });
 
+    const user = await Users.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
@@ -121,8 +118,10 @@ router.post('/login', async (req, res) => {
     if (!passwordMatch) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
-
+  
     const token = jwt.sign({ userId: user._id }, 'seuSegredo');
+
+
     res.status(200).json({ token });
   } catch (error) {
     console.error('Erro ao fazer login:', error);
@@ -132,43 +131,13 @@ router.post('/login', async (req, res) => {
 
 router.get('/usuarios', async (req, res) => {
   try {
-    const data = await Users.find();
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error('Erro ao buscar cursos:', error);
-    return res.status(500).json({ error: 'Erro interno do servidor ao buscar cursos' });
-  }
-});
-
-router.get('/user-data', async (req, res) => {
-  try {
-    const token = req.headers.authorization;
-
-    if (!token) {
-      return res.status(401).json({ message: 'Token de autenticação não fornecido' });
+      const data = await Users.find();
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error('Erro ao buscar cursos:', error);
+      return res.status(500).json({ error: 'Erro interno do servidor ao buscar cursos' });
     }
+})
 
-    const tokenParts = token.split(' ');
-
-    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return res.status(401).json({ message: 'Formato inválido do token de autenticação' });
-    }
-
-    const authToken = tokenParts[1];
-
-    const userId = decodeAuthToken(authToken);
-
-    const userData = await Users.findById(userId);
-
-    if (!userData) {
-      return res.status(404).json({ message: 'Dados do aluno não encontrados' });
-    }
-
-    res.status(200).json(userData);
-  } catch (error) {
-    console.error('Erro ao buscar dados do aluno:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
 
 export default router;
